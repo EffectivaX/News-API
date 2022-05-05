2e5007efae54475bd7e52c01420c09eaf6f51310
Google Jr <andilembele020@gmail.com>
2022-05-05 23:46:12 +0200

    Done done ::final

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ParticlesBg from "particles-bg";
 function App() {
   return (
     <div className="App">
-      <ParticlesBg type="cobweb" bg={true} />
+      <ParticlesBg type="polygon" bg={true} />
       <Articles />
     </div>
   );
